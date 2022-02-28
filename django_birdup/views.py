f2df5d08da813f432b4f95bba1dba5ead6568a51
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-02-28 14:41:23 +0100

    Fixed abs path

--- django_birdup/views.py
+++ django_birdup/views.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, sep
 from os.path import isfile, join
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -10,7 +10,7 @@ def birdup_home(request):
 
     logging.info("!!!!")
     # Test reading listing files from EOS
-    rootpath = join('eos', 'user', 'b', 'birdup', 'test')
+    rootpath = join(sep, 'eos', 'user', 'b', 'birdup', 'test')
     logging.info(listdir(rootpath))
 
     return render(request, template_name="django_birdup/birdup_home.html")
