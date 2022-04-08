0b8ba7193eae7fffa1ecbcc17764f8ad0f75fab5
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-04-08 19:43:42 +0200

    Test with s2i

---
+++ openshift-start-up-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python manage.py collectstatic --noinput
+# python manage.py migrate --run-syncdb
+
+daphne -b 0.0.0.0 -p 8080 django_test.asgi:application
+# gunicorn --bind=0.0.0.0:8080 mlp.wsgi
+# python manage.py runserver 0.0.0.0:8080
