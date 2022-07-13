8e865de96fde97508445985057ef189d002ead27
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-13 16:20:00 +0200

    Updated consumer, added redis

--- django_test/asgi.py
+++ django_test/asgi.py
@@ -11,6 +11,10 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_test.settings")
 
@@ -19,6 +23,8 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     }
 )
