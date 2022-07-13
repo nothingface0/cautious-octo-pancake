194b48bf30143c7db35c394d4564ed811d4515ea
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-13 12:51:48 +0200

    Formatting, channels added

--- django_test/settings.py
+++ django_test/settings.py
@@ -20,74 +20,78 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
-DJANGO_SECRET_ACC = config('DJANGO_SECRET_ACC', default='admin')
-DJANGO_SECRET_PASS = config('DJANGO_SECRET_PASS', default='admin')
+SECRET_KEY = config("DJANGO_SECRET_KEY")
+DJANGO_SECRET_ACC = config("DJANGO_SECRET_ACC", default="admin")
+DJANGO_SECRET_PASS = config("DJANGO_SECRET_PASS", default="admin")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DJANGO_DEBUG', default=False)
+DEBUG = config("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = [
-    config('DJANGO_ALLOWED_HOSTS', default='localhost'),
-    '127.0.0.1',
+    config("DJANGO_ALLOWED_HOSTS", default="localhost"),
+    "127.0.0.1",
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin', 'django.contrib.auth',
-    'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'django_test',
-    'lidar_components', 'django_birdup', 'django_tables2', 'tables_tutorial'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_test",
+    "lidar_components",
+    "django_birdup",
+    "django_tables2",
+    "tables_tutorial",
+    "channels",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'django_test.urls'
+ROOT_URLCONF = "django_test.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django_test.context_processors.global_context'
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django_test.context_processors.global_context",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'django_test.wsgi.application'
+WSGI_APPLICATION = "django_test.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE':
-        config('DJANGO_DATABASE_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME':
-        config('DJANGO_DATABASE_NAME', default='db.sqlite3'),
-        'USER':
-        config('DJANGO_DATABASE_USER', default=''),
-        'PASSWORD':
-        config('DJANGO_DATABASE_PASSWORD', default=''),
-        'HOST':
-        config('DJANGO_DATABASE_HOST', default=''),
-        'PORT':
-        config('DJANGO_DATABASE_PORT', default=''),
+    "default": {
+        "ENGINE": config(
+            "DJANGO_DATABASE_ENGINE", default="django.db.backends.sqlite3"
+        ),
+        "NAME": config("DJANGO_DATABASE_NAME", default="db.sqlite3"),
+        "USER": config("DJANGO_DATABASE_USER", default=""),
+        "PASSWORD": config("DJANGO_DATABASE_PASSWORD", default=""),
+        "HOST": config("DJANGO_DATABASE_HOST", default=""),
+        "PORT": config("DJANGO_DATABASE_PORT", default=""),
     },
 }
 
@@ -96,29 +100,25 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -129,30 +129,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "verbose"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG" if DEBUG else "WARNING",
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} - {asctime} - {module} - {message}",
+            "style": "{",
         },
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG' if DEBUG else 'WARNING',
-    },
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} - {asctime} - {module} - {message}',
-            'style': '{',
-        },
-    }
 }
