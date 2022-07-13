bf14a8a218bf9b24ec04c3d811c1ae71e0160b66
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-13 16:53:42 +0200

    Cleanup

--- django_test/asgi.py
+++ django_test/asgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
