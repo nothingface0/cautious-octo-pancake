4129bac1ff3564b95feae315ca76e5284e6bd54f
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-14 16:39:37 +0200

    Fixed static root

--- django_test/settings.py
+++ django_test/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "/sock/asgi/static")
+STATIC_ROOT = BASE_DIR / "sock" / "asgi" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
