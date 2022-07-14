e582e9b86dad21c73bf76cfd6700fa757021f9cd
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-14 16:34:23 +0200

    Added migrations on startup

--- openshift-start-up-script.sh
+++ openshift-start-up-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
-# python manage.py migrate --run-syncdb
+python manage.py migrate --run-syncdb
 
 daphne -b 0.0.0.0 -p 8080 django_test.asgi:application
 # gunicorn --bind=0.0.0.0:8080 mlp.wsgi
